85575b08ed846e5d59ca892d9615821f4973f766
Dominik Korsa <dominik.korsa@gmail.com>
2021-10-09 19:07:23 +0200

    Add question set patch and list endpoints

--- api-schemas/src/routes/question-sets.ts
+++ api-schemas/src/routes/question-sets.ts
@@ -1,5 +1,10 @@
 import { Static, Type } from '@sinclair/typebox';
-import { trimmedStringSchema } from '../common';
+import { emptyReplySchema, trimmedStringSchema } from '../common';
+
+export const questionSetParamsSchema = Type.Object({
+  setShortId: Type.String(),
+});
+export type QuestionSetParams = Static<typeof questionSetParamsSchema>;
 
 export const createQuestionSetBodySchema = Type.Object({
   name: trimmedStringSchema(),
@@ -9,3 +14,21 @@ export const createQuestionSetReplySchema = Type.Object({
   shortId: Type.String(),
 });
 export type CreateQuestionSetReply = Static<typeof createQuestionSetReplySchema>;
+
+export const questionSetSchema = Type.Object({
+  shortId: Type.String(),
+  name: Type.String(),
+});
+export type QuestionSet = Static<typeof questionSetSchema>;
+
+export const listQuestionSetsReplySchema = Type.Object({
+  questionSets: Type.Array(questionSetSchema),
+});
+export type ListQuestionSetsReply = Static<typeof listQuestionSetsReplySchema>;
+
+export const patchQuestionSetBodySchema = Type.Partial(Type.Object({
+  name: trimmedStringSchema(),
+}));
+export type PatchQuestionSetBody = Static<typeof patchQuestionSetBodySchema>;
+export const patchQuestionSetReplySchema = emptyReplySchema;
+export type PatchQuestionSetReply = Static<typeof patchQuestionSetReplySchema>;
--- backend/src/routes/api/question-sets.ts
+++ backend/src/routes/api/question-sets.ts
@@ -2,7 +2,14 @@ import { FastifyInstance } from 'fastify';
 import {
   CreateQuestionSetBody,
   createQuestionSetBodySchema,
-  CreateQuestionSetReply, createQuestionSetReplySchema,
+  CreateQuestionSetReply,
+  createQuestionSetReplySchema,
+  ListQuestionSetsReply,
+  listQuestionSetsReplySchema,
+  PatchQuestionSetBody, patchQuestionSetBodySchema,
+  PatchQuestionSetReply, patchQuestionSetReplySchema,
+  QuestionSetParams,
+  questionSetParamsSchema,
 } from 'greatest-api-schemas';
 import { nanoid } from 'nanoid';
 import { DbManager } from '../../database/database';
@@ -30,5 +37,57 @@ export function registerQuestionSets(apiInstance: FastifyInstance, dbManager: Db
     return {
       shortId,
     };
+  });
+
+  apiInstance.get<{
+    Reply: ListQuestionSetsReply,
+  }>('/question-sets/list', {
+    schema: {
+      response: {
+        200: listQuestionSetsReplySchema,
+      },
+    },
+  }, async (request) => {
+    const user = await requireAuthentication(request, dbManager, true);
+    return {
+      questionSets: await dbManager.questionSetsCollection
+        .find({
+          ownerId: user._id,
+        })
+        .map((set) => ({
+          shortId: set.shortId,
+          name: set.name,
+        }))
+        .toArray(),
+    };
+  });
+
+  apiInstance.patch<{
+    Params: QuestionSetParams,
+    Body: PatchQuestionSetBody,
+    Reply: PatchQuestionSetReply,
+  }>('/question-sets/:setShortId', {
+    schema: {
+      params: questionSetParamsSchema,
+      body: patchQuestionSetBodySchema,
+      response: {
+        200: patchQuestionSetReplySchema,
+      },
+    },
+  }, async (request) => {
+    const user = await requireAuthentication(request, dbManager, true);
+    const questionSet = await dbManager.questionSetsCollection.findOne({
+      shortId: request.params.setShortId,
+    });
+    if (questionSet === null) return apiInstance.httpErrors.notFound('Question set not found');
+    if (!questionSet.ownerId.equals(user._id)) throw apiInstance.httpErrors.forbidden();
+    await dbManager.questionSetsCollection.updateOne({
+      _id: questionSet._id,
+    }, {
+      $set: {
+        name: request.body.name,
+      },
+    });
+    return {};
   });
 }
