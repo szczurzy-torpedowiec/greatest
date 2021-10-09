315645a0e3331c9ad6f9679c0ebb7b82ba4f9fa8
Dominik Korsa <dominik.korsa@gmail.com>
2021-10-09 19:13:59 +0200

    Add website API functions for question sets

--- website/src/api/index.ts
+++ website/src/api/index.ts
@@ -1,3 +1,8 @@
-export function getUserInfo() {
-  // return ky.get('/api/viewer').json<GetViewerReply>();
+import { GetViewerReply } from 'greatest-api-schemas';
+import ky from 'ky';
+
+export function getViewer() {
+  return ky.get('/api/viewer').json<GetViewerReply>();
 }
+
+export * from './question-sets';
