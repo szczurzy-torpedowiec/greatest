c2e65a39c62332a23a684945d29d64d61976788e
doteq <doteeqq@gmail.com>
2021-10-12 21:08:44 +0200

    Add drawer

--- website/src/components/questionSets/QuestionSetListElement.vue
+++ website/src/components/questionSets/QuestionSetListElement.vue
@@ -102,7 +102,6 @@ export default defineComponent({
       await router.push(`/question-sets/${props.id}/edit`);
     }
 
-    // TODO: Fix rename
     async function submitName() {
       await patchQuestionSet(props.id, {
         name: newName.value.trim(),
