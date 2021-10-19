24c552da32c632828924cdf92f6341db43a4128d
doteq <doteeqq@gmail.com>
2021-10-19 22:18:45 +0200

    Create layout for unauthenticated users

---
+++ website/src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout
+    view="hHh Lpr lFf"
+  >
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+} from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+});
+</script>
+
+<style scoped>
+
+</style>
--- website/src/router/routes.ts
+++ website/src/router/routes.ts
@@ -3,7 +3,13 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('pages/Index.vue'),
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('pages/Index.vue'),
+      },
+    ],
   },
   {
     path: '/teacher',
