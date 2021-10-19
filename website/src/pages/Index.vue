c0302acc328972a2348f6863759b2df528c514e9
doteq <doteeqq@gmail.com>
2021-10-19 22:07:26 +0200

    Move error message to the new Index page

--- website/src/pages/Index.vue
+++ website/src/pages/Index.vue
@@ -4,10 +4,27 @@
 
 <script lang="ts">
 import {
-  defineComponent,
+  defineComponent, onMounted,
 } from 'vue';
+import { useRoute } from 'vue-router';
+import { useQuasar } from 'quasar';
+import { useI18n } from 'vue-i18n';
 
 export default defineComponent({
   name: 'PageIndex',
+  setup() {
+    const route = useRoute();
+    const quasar = useQuasar();
+    const i18n = useI18n();
+
+    onMounted(() => {
+      if (route.query.error === 'authRequired') {
+        quasar.notify({
+          type: 'negative',
+          message: i18n.t('signIn.authRequired'),
+        });
+      }
+    });
+  },
 });
 </script>
