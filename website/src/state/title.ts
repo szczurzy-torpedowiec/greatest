9d46964af371d2bb4c7a725293afead240109142
Dominik Korsa <dominik.korsa@gmail.com>
2021-10-24 17:51:41 +0200

    Remove toolbar router link, which caused errors

--- website/src/state/title.ts
+++ website/src/state/title.ts
@@ -5,8 +5,8 @@ export const TitleLoading = Symbol('Title loading');
 export const titleState = ref<null | string| typeof TitleLoading>(null);
 
 export const useTitleState = (state: DeepReadonly<Ref<string | null | typeof TitleLoading>>) => {
-  watch(state, () => {
-    titleState.value = state.value;
+  watch(state, (value) => {
+    titleState.value = value;
   }, {
     immediate: true,
   });
